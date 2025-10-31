f4e0fb97491913a2b77f87940aebe13f62f501aa
Zach Griffith <zagrdev@gmail.com>
2025-10-31 10:17:24 -0600

    Fri Oct 31 10:17:24 AM MDT 2025

--- scripts/.local/bin/gacp.sh
+++ scripts/.local/bin/gacp.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [[ -z "$1" ]]; then
-  git commit -am "$(date)" && git push
+  git commit -am "$(date)" && git push # push with generic date message
 else
-  git commit -am $1 && git push
+  git commit -am "$1" && git push # push with specified message
 fi
 
 
